0df67454d5bab4b60ba982f0307fc09b67ca15f6
Croman <52852455+CRomanIA@users.noreply.github.com>
2020-04-10 22:53:07 -0400

    secc06_curso

---
+++ Secc06_Creando_API_RestFul_con_express/app.js
@@ -0,0 +1,42 @@
+//crear elemento con la dependencia
+const express = require('express');
+//para instanciar (que puede recibir parametro) el elemento
+const app = express();
+//indicar a la app cuales van a ser los metodos que puedo implementar y su ruta
+/*
+app.get(); //peticion
+app.post(); //envio de datos desde el servidor
+app.put(); //actualizacion
+app.delete(); //eliminacion
+*/
+
+//manejo de solicitudes http get:
+const usuarios = [
+    {id:1, nombre:'Felipe'},
+    {id:2, nombre:'Pablo'},
+    {id:3, nombre:'Cony'}
+];
+
+//promesa: request, response:
+app.get('/', (req, res) => {
+    res.send('hola mundo desde express.');
+});
+
+app.get('/api/usuarios', (req, res) => {
+    res.send(['seba','felipe','gillian','kesly'])
+});
+
+app.get('api/usuarios/:id', (req, res) => {
+    let usuario = usuarios.find(u => u.id === parseInt(req.params.id));
+    if(!usuario) res.status(404).send('El usuario no fue encontrado');
+    res.send(usuario);
+    //res.send(req.params.id);
+});
+
+//variable de entorno...
+const port = process.env.PORT || 5000;
+//indicar sobre que puerto va a escuchar la peticion que se realizara
+//con esto el servidor web se esta ejecutando, y escucha a traves del puerto 5000 de ejemplo
+app.listen(port, () => {
+    console.log(`escuchando desde el puerto ${port}...`);
+});
---
+++ Secc06_Creando_API_RestFul_con_express/notes.js
@@ -0,0 +1,71 @@
+//vamos a crear nuestra propia api rest que va a recibir y atender peticiones
+//desde la aplicaciones cliente o para la aplicacion movil.
+
+/*
+Secc06_cap52:
+¿Qué es un api rest ful?
+es un servicio que se publica en el servidor y que se hacen tranferencia de datos entre el cliente
+y el servidor de forma asincrona, pero hay ciertas operaciones que se van a hacer en el servidor
+a traves del api rest
+
+create = post
+read = get
+update = put
+delete = delete
+*/
+
+/*
+Secc06_cap53:
+Introduccion e instalacion de express:
+express: es un framework que trabaja gracias a node.
+node: es un entorno que trabaja en tiempo de ejecucion, multiplataforma y que nos permite crear
+toda clase de herramientas en el lado del servidor
+api resful: sistema de apis que incluye el protocolo de http y poder servir peticiones a usuarios
+
+para crear el proyecto en express:
+-levanta el proyecto
+npm init --yes
+--intalar express
+npm install express --save
+se crea la dependencia en package.json
+se crea la carpeta de node_modules con la paqueteria de express
+*/
+
+/*
+Secc06_cap54:
+nuestro primer web server:
+dentro de la carpeta del proyecto
+crear archivo app.js
+ir a app.js
+*/
+
+/*
+Secc06_cap55:
+variables de entorno:
+***a escribir...
+
+set PORT=VALOR DEL HOST DE LA VARIABLE
+*/
+
+/*
+Secc06_cap56    
+parametros en las rutas:
+
+para tomar el valor de un parametro es necesario recibir el parametro y solicitar el valor del parametro
+app.get('api/usuario : id', req,res =>{
+    res.send(req.params.id);
+})
+*/
+
+/*
+Secc06_cap57:
+manejo de solicitudes HTTP GET:
+que sabemos?
+gestionar las rutas
+enviar parametros
+
+se verá como se gestionan las peticiones
+veremos hacer un get con http:
+
+*/
+
